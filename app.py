9a2d6ba3ebf116e9bb13d05a0de2a0b29b95cc2e
Elvis Mudamba <elvismudamba@gail.com>
2024-09-12 10:33:18 +0300

    commit

---
+++ app.py
@@ -0,0 +1,16 @@
+print("***************************************************")
+print("Welcome to this game!!!!")
+
+playing = input("Do you want to play? ")
+ 
+if playing != "yes":
+    quit()
+
+print("cool!!! lets play ")
+
+answer = input("What does CPU stands for? ")
+if answer == "central processing unit" :
+    print("correct!!")
+
+else:
+    print("incorrect!")
